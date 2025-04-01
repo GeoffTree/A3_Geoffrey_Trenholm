18ce6e7aeab784f137ca69a2c422733e0f819e2a
Geoff Trenholm <trenholg@sheridancollege.ca>
2025-04-01 16:09:34 -0400

    initial commit

---
+++ src/main/java/ca/sheridancollege/trenholg/a3_geoffrey_trenholm/A3GeoffreyTrenholmApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.trenholg.a3_geoffrey_trenholm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A3GeoffreyTrenholmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A3GeoffreyTrenholmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/sheridancollege/trenholg/a3_geoffrey_trenholm/A3GeoffreyTrenholmApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.trenholg.a3_geoffrey_trenholm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A3GeoffreyTrenholmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
